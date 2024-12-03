0998757ae664b5a2ef3b362bf002ca93879d4bde
N.Drozd@cbt.by <N.Drozd@cbt.by>
2024-12-03 11:42:01 +0300

    fix: redirect middleware doesn't work

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,12 +1,15 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func VerifyCredentials(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//TODO: request to auth service to verify header credentials
+		fmt.Println("retdirecting to localhost:8080/video-list")
+		// http.Redirect(w, r, "file:///E:/Diploma/ui-auth/login.html", http.StatusMovedPermanently)
 		next.ServeHTTP(w, r)
 	})
 }
